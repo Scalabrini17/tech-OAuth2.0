222028e7846103bc02be09ccae319f439aacb1de
Scalabrini17 <scalabriniexecutivo@gmail.com>
2026-03-23 14:59:36 -0300

    add document .env

--- setup/settings.py
+++ setup/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6cn6dt!)y*a2od@f+(+b+3ekf5g!xss=ei2b=vxqo_=dg0616'
+SECRET_KEY = str(os.path.join('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
